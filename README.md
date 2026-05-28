f21deb5897defd0f4202e232a5d963349aee8981
chaitanya <chaitanya@gmail.com>
2026-05-28 18:59:03 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+1. Open backend in STS
+2. Run application
+3. Open frontend in VS Code
+4. npm install
+5. npm run dev
---
+++ backend/src/main/java/com/breatheesg/BreatheApplication.java
@@ -0,0 +1,7 @@
+
+package com.breatheesg;
+public class BreatheApplication {
+ public static void main(String[] args){
+  System.out.println("Run Spring Boot");
+ }
+}
